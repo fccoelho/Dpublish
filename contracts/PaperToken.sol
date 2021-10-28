c942f50a5289d1c2a22a6ad8176350d28d244fb9
RaphaLevy <raphael.levy147@gmail.com>
2021-10-28 10:42:12 -0300

    Adiciona contratos iniciais

---
+++ contracts/PaperToken.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract PaperToken is ERC721, ERC721URIStorage, Ownable {
+    constructor() ERC721("PaperToken", "PTK") {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://dpublish.org";
+    }
+
+    function safeMint(address to, uint256 tokenId) public onlyOwner {
+        _safeMint(to, tokenId);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+}
