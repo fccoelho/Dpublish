77e831a8a9de91fae3187ccd54479463702ead25
tiagodsilva <tdsh97@gmail.com>
2021-12-12 13:23:47 -0300

    I truly hope it is OK now

--- contracts/DPublish.sol
+++ contracts/DPublish.sol
@@ -6,8 +6,32 @@ import "./PaperToken.sol";
 contract DPublish {
     // mapping(string => address) public submitted_manuscripts;
     mapping(address => uint) private balances; 
+	
+    PaperTokens papersMetadata; 
 
-    PaperToken public papers; 
+    function submit_manuscript(string memory idmanuscript) public {
+        require(!papersMetadata.isSubmitted[idmanuscript],
+                "Manuscript already submitted! Wait for a review.");
+
+        PaperToken tk = new PaperToken();
+
+        papersMetadata.manuscripts.push(idmanuscript);
+        uint _id = papersMetadata.manuscripts.length;
+        papersMetadata.isSubmitted[idmanuscript] = true;
+        papersMetadata.submittedManuscripts[address(tk)] = msg.sender;
+        papersMetadata.manuscriptsIdentifier[idmanuscript] = address(tk);
+    }
+
+    function unsubmit_manuscript(string memory idmanuscript) public {
+        require(papersMetadata.isSubmitted[idmanuscript] && checkAuthor(idmanuscript, msg.sender),
+                    "Thou must burn an existing manuscript!");
+        address _id = papersMetadata.manuscriptsIdentifier[idmanuscript];
+        // delete manuscripts[_id]; // Keep!
+        delete papersMetadata.submittedManuscripts[_id];
+        delete papersMetadata.manuscriptsIdentifier[idmanuscript];
+	delete papersMetadata.isSubmitted[idmanuscript]; 
+    }
+
 
     uint internal submission_fee = 100; 	
 
@@ -19,13 +43,5 @@ contract DPublish {
 	    return address(this).balance; 
     } 
 
-    /**
-    * Submit manuscript
-     */
-
-    function submit_manuscript(string memory idmanuscript) public payable { 
-	address author = msg.sender; 
-        require(msg.value >= submission_fee, "there is a submission fee"); 
-    }
 
 }
--- contracts/PaperToken.sol
+++ contracts/PaperToken.sol
@@ -5,43 +5,22 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+struct PaperTokens {
+	string[] manuscripts; 
+	mapping (string => bool) isSubmitted; 
+	mapping (address => address) submittedManuscripts; 
+        mapping (string => address) manuscriptsIdentifier; 
+} 	
+
 contract PaperToken is ERC721, ERC721URIStorage, Ownable {
     constructor() ERC721("PaperToken", "PTK") {}
-	
-    string[] public manuscripts; 
-    mapping (string => bool) isSubmitted; 
-    mapping (uint => address) submittedManuscripts; 
-    mapping (string => uint) manuscriptsIdentifier; 
-
-    function _baseURI() internal pure override returns (string memory) {
+    
+   function _baseURI() internal pure override returns (string memory) {
         return "https://dpublish.org";
     }
 
     function safeMint(address to, uint256 tokenId) public onlyOwner {
         _safeMint(to, tokenId);
-    }
-
-    function mint(address author, string memory idmanuscript) public {
-	require(!isSubmitted[idmanuscript], 
-		"Manuscript already submitted! Wait for a review."); 
-
-	manuscripts.push(idmanuscript); 
-	uint _id = manuscripts.length; 
-	isSubmitted[idmanuscript] = true; 
-	submittedManuscripts[_id] = msg.sender; 
-	manuscriptsIdentifier[idmanuscript] = _id; 
-	_mint(author, _id); 
-    } 
-
-    // The user can get a refund for extracting the paper 
-    function burn(string memory idmanuscript) public {
-	require(isSubmitted[idmanuscript], 
-		    "Thou must burn an existing manuscript!"); 
-	uint _id = manuscriptsIdentifier[idmanuscript]; 
-	delete manuscripts[_id]; 
-	delete submittedManuscripts[_id]; 
-	delete manuscriptsIdentifier[idmanuscript]; 
-	_burn(_id); 
     }
 
     // The following functions are overrides required by Solidity.
