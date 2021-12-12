6cfd3f24c34b357f798f9ebe62a040b820846f70
tiagodsilva <tdsh97@gmail.com>
2021-12-12 10:26:17 -0300

    Implement submission fee

--- contracts/DPublish.sol
+++ contracts/DPublish.sol
@@ -3,12 +3,25 @@ pragma solidity ^0.8.2;
 
 contract DPublish {
     mapping(string => address) public submitted_manuscripts;
+ 
+    uint internal submission_fee = 100; 	
+
+    function setSubmissionFee(uint fee) internal {
+	    submission_fee = fee; 
+    } 
+
+    function getBalance() public view returns (uint) { 
+	    return address(this).balance; 
+    } 
 
     /**
     * Submit manuscript
      */
-    function submit_manuscript(string memory idmanuscript) public payable{
-        submitted_manuscripts[idmanuscript] = msg.sender; 
+
+    function submit_manuscript(string memory idmanuscript) public payable { 
+	address author = msg.sender; 
+        require(msg.value >= submission_fee, "there is a submission fee"); 
+        submitted_manuscripts[idmanuscript] = author; 
     }
 
     function get_submitted_manuscript(string memory idmanuscript) public view returns (address){
