a25d32ef1a039c8c3a0501daa33fb1a1d156779e
tiagodsilva <tdsh97@gmail.com>
2021-12-11 21:20:09 -0300

    Write function to submite paper; it is surprisingly OK

--- contracts/DPublish.sol
+++ contracts/DPublish.sol
@@ -7,9 +7,13 @@ contract DPublish {
     /**
     * Submit manuscript
      */
-    function submit_manuscript(string memory idmanuscript) public view{
-        submitted_manuscripts[idmanuscript]; 
+    function submit_manuscript(string memory idmanuscript, address submit_to) public payable{
+        submitted_manuscripts[idmanuscript] = submit_to; 
     }
+
+    function get_submitted_manuscript(string memory idmanuscript) public view returns (address){
+	    return submitted_manuscripts[idmanuscript]; 
+    } 
 
 
 }
