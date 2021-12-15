d4e867aa4f08ac32802fa4daf138afe587e8c5f6
Victor <70376108+victorbombarda@users.noreply.github.com>
2021-12-15 12:08:47 -0300

    Update ERC20Votes.sol

--- contracts/ERC20Votes.sol
+++ contracts/ERC20Votes.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 // OpenZeppelin Contracts v4.3.2 (token/ERC20/extensions/ERC20Votes.sol)
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
